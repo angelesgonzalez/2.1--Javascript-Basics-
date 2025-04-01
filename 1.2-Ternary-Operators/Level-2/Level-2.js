6a944c3ed4492ceb91ff2ecca5ebb2b2ed08e9f8
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-04-01 12:50:25 +0200

    Ternary Operators: Complete Exercise 1 from Level 2

---
+++ 1.2-Ternary-Operators/Level-2/Level-2.js
@@ -0,0 +1,25 @@
+
+// Exercise 1
+
+const isPositiveOrNegative = (num) => {
+	return num === 0
+		? `${num} is equal to 0`
+		: num > 0
+		? `${num} is a positive number`
+		: `${num} is a negative number`;
+};
+
+console.log(isPositiveOrNegative(-1)); //negative
+console.log(isPositiveOrNegative(2)); //positive
+console.log(isPositiveOrNegative(0)); // 0
+
+
+// !Exercise 2
+// Operador ternari amb funcions: Crea una funció trobarMaxim que accepti tres paràmetres (a, b, c) i utilitzi // l'operador
+// ternari per determinar el valor màxim.
+
+const findMaximum = (num1, num2, num3) => {
+
+
+
+}
