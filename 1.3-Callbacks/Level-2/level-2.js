a0c73f44019578a8c35b8a21ba55b5c609d50a0f
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-04-02 11:02:32 +0200

    Complete Level 2 - Callbacks

--- 1.3-Callbacks/Level-2/level-2.js
+++ 1.3-Callbacks/Level-2/level-2.js
@@ -1,6 +1,25 @@
 // !Exercici 3
-// Ús de callbacks en funcions asíncrones: Escriu una funció esperarISaludar que accepti dos paràmetres: un nom i una funció de callback. La funció ha d'esperar 2 segons i llavors invocar la funció de callback, passant el nom com a paràmetre.
 
+const waitAndSayHi = (personName, callback) => {
+	setTimeout(() => callback(personName), 2000);
+};
+
+waitAndSayHi("Angeles", (personName) => {
+	console.log(`Hi ${personName}`);
+});
 
 // !Exercici 4
-// Callbacks amb arrays: Escriu una funció processarElements que accepti dos paràmetres: un array i una funció de callback. La funció processarElements ha d'invocar la funció de callback per cada element de l'array.
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const processElements = (array, callback) => {
+	let newArray = [];
+	for (let i = 0; i < array.length; i++) {
+		newArray.push(callback(array[i]));
+	}
+	return newArray;
+};
+
+newArray = processElements(arr, (element) => {
+	return ++element;
+});
