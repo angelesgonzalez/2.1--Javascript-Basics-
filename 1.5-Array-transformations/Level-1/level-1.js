4188101cc147cb87e24e1cb933ffed8b1bc11e04
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-04-03 10:47:01 +0200

    Complete Exercise 1 & 2 from Level 1

---
+++ 1.5-Array-transformations/Level-1/level-1.js
@@ -0,0 +1,19 @@
+// !Exercici 1
+// Map: Teniu un array de números [1, 2, 3, 4]. Crea una nova array que contingui el quadrat de cada número.
+
+const numbers = [1, 2, 3, 4];
+const squareNumbers = numbers.map((number) => number * number);
+
+// !Exercici 2
+// Filter: Teniu una array de números [1, 2, 3, 4]. Crea una nova array que només contingui els números parells.
+
+const numbersToFilter = [1, 2, 3, 4];
+const evenNumbers = numbers.filter((num) => {
+if (num % 2 === 0) return num;
+});
+
+// !Exercici 3
+// Find: Teniu una array de números [1, 10 , 8, 11]. Utilitza la funció find per a trobar el primer número que és major a 10.
+
+// !Exercici 4
+// Reduce: Teniu una array de números [13, 7, 8, 21]. Fes servir la funció reduce per a calcular la suma total dels números.
