f22f97dba2be1a5c0ebcdcf65267807af1dc8256
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-04-03 10:21:36 +0200

    Update files after completing Exercise 6, level 3

--- 1.4-Spread-and-Rest/Level-3/level-3.js
+++ 1.4-Spread-and-Rest/Level-3/level-3.js
@@ -1,15 +1,27 @@
 // !Exercise
 
-const names = ['Angeles', 'Jordi', 'Laura'];
+const names = ["Angeles", "Jordi", "Laura"];
 
 const printNames = (name1, name2, name3) => {
-    console.log(name1); 
-    console.log(name2); 
-    console.log(name3); 
-}
+	console.log(name1);
+	console.log(name2);
+	console.log(name3);
+};
 
 printNames(...names);
 
+// !Exercise 6
 
-// !Exercici 6
-// Fusionant Objectes amb Spread: Crea dos objectes amb propietats diferents. Utilitza l'operador spread per a fusionar aquests dos objectes en un de nou.
+const user = {
+	id: 1,
+	name: "Alex",
+	age: 28,
+	email: "alex@example.com",
+};
+
+const settings = {
+	theme: "dark",
+	notifications: true,
+};
+
+const newUser = { ...user, ...settings };
