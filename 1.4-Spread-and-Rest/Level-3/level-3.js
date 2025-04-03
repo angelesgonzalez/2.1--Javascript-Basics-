c3df3901447f1a73e04c6e7097eece21c6b39522
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-04-03 10:16:48 +0200

    Complete Exercise 5, level 3

--- 1.4-Spread-and-Rest/Level-3/level-3.js
+++ 1.4-Spread-and-Rest/Level-3/level-3.js
@@ -1,5 +1,15 @@
-// !Exercici 5
-// Spread en Funcions: Crea una funció que accepti tres arguments. Després, crea una array amb tres elements i crida la funció utilitzant l'operador spread amb aquesta array.
+// !Exercise
+
+const names = ['Angeles', 'Jordi', 'Laura'];
+
+const printNames = (name1, name2, name3) => {
+    console.log(name1); 
+    console.log(name2); 
+    console.log(name3); 
+}
+
+printNames(...names);
+
 
 // !Exercici 6
 // Fusionant Objectes amb Spread: Crea dos objectes amb propietats diferents. Utilitza l'operador spread per a fusionar aquests dos objectes en un de nou.
