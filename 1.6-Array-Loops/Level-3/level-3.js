5d43899d5a6670bb234003ac3ecfb4ddb7629012
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-04-04 15:03:09 +0200

    Complete Level 3

--- 1.6-Array-Loops/Level-3/level-3.js
+++ 1.6-Array-Loops/Level-3/level-3.js
@@ -1,2 +1,5 @@
 // !Exercici 6
-// for-of amb index: Utilitza un bucle for-of per a imprimir a la consola cada element de l'array i la seva posició (index): let noms = ['Anna', 'Bernat', 'Clara']
+const names = ["Anna", "Bernat", "Clara"];
+for (let [index, firstName] of names.entries()) {
+	console.log(index, firstName);
+}
